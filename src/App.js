fa6a8da279e0ae7307d678a23559f95305f9d729
mihajlovskakate <mihajlovskakate1@gmail.com>
2022-12-02 12:15:25 +0100

    Initial commit
    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects"
+import Home from "./components/Home"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Contact from "./components/Contact/Contact"
+import ThankYou from "./components/Contact/ThankYou"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   return (
+    <Router>
+    <Navbar/>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />  
+      <Route path="/contact" element={<Contact />} />   
+      <Route path="/thankyou" element={<ThankYou />} /> 
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
