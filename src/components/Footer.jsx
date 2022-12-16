8c33748bce8c77e05f564f5811de9952b78e92ad
Sinisha Mihajlovski <52503136+sinisha-mihajlovski@users.noreply.github.com>
2022-12-16 16:00:10 +0000

    Changed the link to point to Katerinas github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
         <div className = "footer">
         <div className="social-network-icons">
                             <a className="social-link" href="https://www.linkedin.com/in/kmihajlovska/"><i className="fa-brands fa-linkedin fa-2x"></i></a>
-                            <a className="social-link" href="https://www.appbrewery.co/"><i className="fa-brands fa-square-github fa-2x"></i></a>
+                            <a className="social-link" href="https://github.com/mihajlovskakate"><i className="fa-brands fa-square-github fa-2x"></i></a>
                         </div>
             <div className="main-footer-lower">
             <p>© 2022. Made by Katerina Mihajlovska.</p>
